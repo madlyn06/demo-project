a44596e2e2906f85a6d6d8ead838e0395b3c2f3e
Nguyễn Mạnh Hồng <hongnmp@gmail.com>
2024-07-28 13:18:58 +0700

    update code

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -4,6 +4,9 @@ import fb from 'src/assets/fb.png'
 import chim from 'src/assets/chim.png'
 import iabc from 'src/assets/in.png'
 import inta from 'src/assets/inta.png'
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
+import { BiLogoLinkedinSquare } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 const logo = [
   {
     color: '#3b5999',
@@ -44,27 +47,52 @@ export default function Footer() {
           <div>
             <p className='md:text-3xl text-2xl  text-[#c0854f] font-bold cursor-pointer font-copper'>Unix</p>
           </div>
-          <div className='flex items-center justify-center'>
+          <div className='flex items-center mt-2 justify-center'>
             <div
               style={{
-                clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)'
+                transform: 'skew(-40deg)'
               }}
-              className='bg-[#353735] font-copper font-bold text-center text-white text-xl inline-block px-4 md:px-16  md:py-1'
+              className={`bg-[#323432] mr-2 text-center text-white text-xl inline-block px-4 md:px-8 md:py-1`}
             >
-              Mail
+              <p className='text-sm' style={{ transform: 'skew(40deg)' }}>
+                Mail
+              </p>
             </div>
-            {logo.map((item, index) => (
-              <div
-                key={index}
-                style={{
-                  clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)',
-                  backgroundColor: `${item.color}`
-                }}
-                className={`bg-[${item.color}] text-center text-white text-xl inline-block px-4 md:px-16 md:py-1`}
-              >
-                <img src={item.name} alt='' />
-              </div>
-            ))}
+            <Link
+              to='https://www.facebook.com/Unixvisual'
+              style={{
+                transform: 'skew(-40deg)'
+              }}
+              className={`bg-[#3b5996] text-center text-white text-xl inline-block px-4 md:px-8 md:py-1`}
+            >
+              <FaFacebookF style={{ transform: 'skew(40deg)' }} />
+            </Link>
+            <div
+              style={{
+                transform: 'skew(-40deg)'
+              }}
+              className={`bg-[#5daff0] text-center text-white text-xl inline-block px-4 md:px-8 md:py-1`}
+            >
+              <FaTwitter style={{ transform: 'skew(40deg)' }} />
+            </div>
+            <Link
+              to='https://www.linkedin.com/in/alex-nguyen-025234293'
+              style={{
+                transform: 'skew(-40deg)'
+              }}
+              className={`bg-[#007bbf] text-center text-white text-xl inline-block px-4 md:px-8 md:py-1`}
+            >
+              <BiLogoLinkedinSquare style={{ transform: 'skew(40deg)' }} />
+            </Link>
+            <Link
+              to='https://www.instagram.com/johan.unixvisual'
+              style={{
+                transform: 'skew(-40deg)'
+              }}
+              className={`bg-[#714f40] text-center text-white text-xl inline-block px-4 md:px-8 md:py-1`}
+            >
+              <FaInstagram style={{ transform: 'skew(40deg)' }} />
+            </Link>
           </div>
         </div>
       </div>
